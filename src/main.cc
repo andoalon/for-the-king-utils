ad257095e18faa4e6ff36cb4b69c7ba68f50105a
andoalon <andoalon97@gmail.com>
2020-06-01 23:46:55 +0200

    Add option to enable link time optimization. Lower cmake_minimum_required if not compiling with MSVC

--- src/main.cc
+++ src/main.cc
@@ -19,6 +19,7 @@
 #ifdef _MSC_VER
 #	pragma warning(push)
 #	pragma warning(disable : 4127) // Conditional expression is constant
+#	pragma warning(disable : 4100) // unreferenced formal parameter
 #endif
 #include "attacks.pb.h"
 #ifdef _MSC_VER
